e5ef363abecd565f903fe6febc10e59698115cd7
Truptiit <97502452+Truptiit@users.noreply.github.com>
2022-02-25 19:13:34 +0530

    Add files via upload

---
+++ assignmnet_10/database/migrations/2022_02_23_171815_create_product_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->bigIncrements('product_category_id');
+            $table->string('product_category_name',50)->nullable();
+            $table->string('product_description')->nullable();
+            $table->enum('status',array('Active','Inactive'))->default('Active');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+};
